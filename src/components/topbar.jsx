64d008d6ebde6c2a00928c198f3d4b54fd26523e
Distant22 <sindistant@gmail.com>
2023-05-02 00:57:18 +0800

    feat(topbar): add link to root

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function TopBar() {
     return (
       <>
         <div class="h-32 bg-topbar flex items-center ">
-            <div class="ml-10 text-3xl font-bold">
-                你的 AI 旅遊小幫手
-            </div>
+          <Link to={{pathname: '/'}} class="ml-10 text-3xl font-bold">
+          你的 AI 旅遊小幫手
+          </Link>
         </div>
       </>
     );
