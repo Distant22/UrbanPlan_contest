62e4f363d4ca821dfda2d07008f5badc4bb2dc5d
Distant22 <sindistant@gmail.com>
2023-05-04 11:14:01 +0800

    feat(getJourney): setup firestore query for journey

--- src/components/getJourney.jsx
+++ src/components/getJourney.jsx
@@ -3,7 +3,7 @@ import { getDB } from "../firebase";
 
 export async function getJourney(name) {
   const journeys = collection(getDB(), 'journey2023');
-  const journeyQuery = query(journeys, where('Region', '==', name), limit(10));
+  const journeyQuery = query(journeys, where('Region', '==', name), limit(30));
   const journeySnapShot = await getDocs(journeyQuery);
   const journeyList = journeySnapShot.docs.map(doc => doc.data());
   
