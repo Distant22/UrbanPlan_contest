26ba666e1144c79f7e6cb1fb9f615861cb8f8531
Distant22 <sindistant@gmail.com>
2023-05-04 11:14:25 +0800

    feat(getHotel): setup firestore query for hotel

---
+++ src/components/getHotel.jsx
@@ -0,0 +1,20 @@
+import { getDocs, collection, query, where, limit } from "firebase/firestore"; 
+import { getDB } from "../firebase";
+
+export async function getHotel(name) {
+  const journeys = collection(getDB(), 'hotel2023');
+  const journeyQuery = query(journeys, where('Region', '==', name), limit(30));
+  const journeySnapShot = await getDocs(journeyQuery);
+  const journeyList = journeySnapShot.docs.map(doc => doc.data());
+  
+  return journeyList;
+}
+
+export async function getTypeHotel(region, hotelType) {
+  const journeys = collection(getDB(), 'hotel2023');
+  const journeyQuery = query(journeys, where('Region', '==', region),  where('Class', '==', hotelType), limit(30));
+  const journeySnapShot = await getDocs(journeyQuery);
+  const journeyList = journeySnapShot.docs.map(doc => doc.data());
+  
+  return journeyList;
+}
